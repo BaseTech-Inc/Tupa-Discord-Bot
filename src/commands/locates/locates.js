e09a3d436ce47bc0793ac7d97f219a3ed2f0f215
Guilherme Ivo <guilhermeivob@gmail.com>
2021-11-07 06:56:25 -0300

    Adicionado LocalidadesService

--- src/commands/locates/locates.js
+++ src/commands/locates/locates.js
@@ -1,26 +1,87 @@
 'use strict'
 
 import LocalidadesService from '../../services/LocalidadesService.js'
+import { MessageActionRow, MessageButton, MessageEmbed } from 'discord.js'
 
 export default (() => {   
 
     let message = async (msg, args) => {
         if (args.length > 0) {
-            if (typeof args[0] === 'object') {
-                args.map((arg, index) => args[index] = arg.value)
+            let nomePais = ''
+            let nomeEstado = ''
+            let nomeCidade = ''
+            let nomeBairro = ''
+
+            args.forEach(current => {
+                if (current.name == 'bairro') {
+                    nomeBairro = current != null ? current.value : ''
+                } else if (current.name == 'cidade') {
+                    nomeCidade = current != null ? current.value : ''
+                } else if (current.name == 'estado') {
+                    nomeEstado = current != null ? current.value : ''
+                } else if (current.name == 'pais') {
+                    nomePais = current != null ? current.value : ''
+                }
+            })
+
+            let responseLocalidades = await LocalidadesService.processLocalidades(
+                nomePais, 
+                nomeEstado, 
+                nomeCidade, 
+                nomeBairro)
+
+            if (responseLocalidades.succeeded) {
+                const embed = new MessageEmbed()
+
+                embed
+                    .setColor('#2484f3')
+                    .setTitle('Lista de localidades')
+                    .setURL('http://tupaweb.azurewebsites.net/')
+                    .setTimestamp()
+	                .setFooter('5/' + responseLocalidades.data.length);
+
+                let count = 1
+
+                responseLocalidades.data.forEach(currentData => {
+                    if (count < 7) {
+                        let address = responseLocalidades.data[count].nome + ', ' + responseLocalidades.data[count].cidade.nome + ' - ' + responseLocalidades.data[count].cidade.estado.sigla
+
+                        embed.addField(
+                            '\u200B',
+                            address,
+                            true
+                        )
+                    }
+
+                    count++
+                })
+
+                embed.addField(
+                    '\u200B',
+                    '\u200B',
+                    false
+                )
+
+                const row = new MessageActionRow()
+                    .addComponents(
+                        new MessageButton()
+                            .setCustomId('previousPage')
+                            .setLabel('Página anterior')
+                            .setStyle('SECONDARY')
+                            .setDisabled(true))
+                    .addComponents(
+                        new MessageButton()
+                            .setCustomId('nextPage')
+                            .setLabel('Próxima página')
+                            .setStyle('PRIMARY'))
+
+                return { embeds: [embed], components: [row] }
+            } else {
+                return { content: 'Ocorreu um erro, tente novamente mais tarde. 😥' }
             }
-
-            let nomePais = args[0] != null ? args[0] : ''
-            let nomeEstado = args[1] != null ? args[1] : ''
-            let nomeCidade = args[2] != null ? args[2] : ''
-            let nomeBairro = args[3] != null ? args[3] : ''
-
-            console.log(nomePais, nomeEstado, nomeCidade, nomeBairro)
-
-            await LocalidadesService.processLocalidades()
         }
 
-        return { content: 'Pong!' }
+        return { content: 'É nescessário passar alguma informação.' }
     }  
     
     let help = () => {
--- src/services/LocalidadesService.js
+++ src/services/LocalidadesService.js
@@ -6,22 +6,20 @@ export default (() => {
 
     const baseUrl = 'https://tupaserver.azurewebsites.net/api/v1/Localidades'
 
-    const processLocalidades = async () => {
+    const processLocalidades = async (pais, estado, cidade, bairro) => {
         const options = {
             method: 'get',
             headers: {
-                Authorization: 'bearer 1234'
+                Authorization: `bearer ${ bearerToken }`
             }
         }
 
-        fetch(baseUrl, options)
-            .then(response => response.json())
-            .then(json => {
-                console.log(json)
-            })
-            .catch((err) => { 
-                console.error(err)
-            })
+        let url = baseUrl + `?namePais=${ pais }&nameEstado=${ estado }&nameCidade=${ cidade }&nameDistrito=${ bairro }`
+
+        const response = await fetch(url, options);
+        const json = await response.json();
+
+        return json
     }
 
     return {
